d0ebac5df2ccaf3b2804541e585f58ced2d80730
Utkarsh Koppikar <47137566+utkarshkoppikar7@users.noreply.github.com>
2021-06-07 11:33:09 +0530

    Add files via upload

--- models/posts.js
+++ models/posts.js
@@ -10,6 +10,10 @@ const postSchema = new Schema(
         content:{
             type: String,
             required: true
+        },
+        image:{
+            type: String,
+            required: false
         }
     },
     {
